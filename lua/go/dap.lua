0c739b9aa295d757c3ef7bc03d2a67a0157a91fc
Davincible <david.brouwer.99@gmail.com>
2022-07-01 02:36:43 +0200

    fix(dap): nil check closure of stderr stdout handle

--- lua/go/dap.lua
+++ lua/go/dap.lua
@@ -349,9 +349,10 @@ M.run = function(...)
         end)
       end
 
-      stdout:close()
-      stderr:close()
-      handle:close()
+      _ = stdout and stdout:close()
+      _ = stderr and stderr:close()
+      _ = handle and handle:close()
+
       stdout = nil
       stderr = nil
       handle = nil
